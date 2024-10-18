a4477f9111e41f224acc0a091cabaf31354dac96
Nanakwamespeed <kwamekrah48@gmail.com>
2024-10-18 14:47:46 +0000

    added required assets to the assets folder and a few resources for material design

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import MaterialDashboard from "./material-dashboard";
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(MaterialDashboard);
 app.mount('#app');
