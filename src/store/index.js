99a2c9394fb7fb53e4ce1f821b9a9b788891cdfe
Nanakwamespeed <kwamekrah48@gmail.com>
2024-10-18 13:35:50 +0000

    added router and store for payroll system and included them in main js

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+      count: 0
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      commit('increment');
+    }
+  },
+  getters: {
+    count: (state) => state.count
+  }
+});
+
+export default store;
